430335da1f826e859ff51a26812e8ca1f26c22a5
vedangj044 <vedangj044@gmail.com>
2019-12-02 16:36:51 +0530

    CHhanged pep8

--- app/callAPI.py
+++ app/callAPI.py
@@ -1,29 +1,32 @@
 from googleapiclient.discovery import build
 import requests
 
+
 def Youtube(credentials1):
     finalList = []
 
-    #call the api to retieve the list of liked videos
+# call the api to retieve the list of liked videos
     youtube = build('youtube', 'v3', credentials=credentials1)
     resp = youtube.videos().list(part="snippet", myRating="like").execute()
 
-    #traverser the response by calling the api again with page token
+# traverser the response by calling the api again with page token
     while("nextPageToken" in resp):
         for i in resp["items"]:
-        #check for category of music and entertainment
-            if i["snippet"]["categoryId"]=='24' or i["snippet"]["categoryId"]=='10':
+            # check for category of music and entertainment
+            pepcor = i["snippet"]["categoryId"]
+            if(pepcor == '24' or pepcor == '10'):
                 finalList.append(i['id'])
 
         resp = youtube.videos().list(part="snippet",
-                                    myRating="like",
-                                    pageToken=resp["nextPageToken"]).execute()
+                                     myRating="like",
+                                     pageToken=resp["nextPageToken"]).execute()
 
     return finalList
 
 
 def userInfo(credentials):
-    #url = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json"
-    url = "https://www.googleapis.com/oauth2/v1/userinfo?access_token="+str(credentials.token)
+    # url = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json"
+    url = "https://www.googleapis.com/oauth2/v1/userinfo?access_token="+
+    str(credentials.token)
     resp = requests.get(url)
     print(resp.text)
